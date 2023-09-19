6bfae6f3845b8a05fc44fb32430c8372d09acf6f
naikvaditya <142937246+naikvaditya@users.noreply.github.com>
2023-09-19 15:43:17 -0500

    generic commit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 git status
 git add .
+git status
 git commit -m 'generic commit'
+git status
 git pull origin master
 git push origin master
+echo "--------------------- DONE ---------------------"
